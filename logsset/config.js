e43bd62b4e35903353c1dc244776bfacaab268eb
ssantiago16 <santiago@issivs.com>
2021-09-20 19:14:07 -0500

    se modifican condiciones por degecto para los logs

--- logsset/config.js
+++ logsset/config.js
@@ -1,4 +1,4 @@
-// Log Settings Thermal GUI
+// Log Settings Integration server
 //By default:
 //exports.logs_amount = '3';
 //exports.log_size = '1240';
@@ -8,6 +8,6 @@
 //level, Level of logs to show, this option allows: TRACE, DEBUG, INFO, ERROR and WARN
 
 
-exports.logs_amount = '3';
-exports.log_size = '124000';
-exports.level = 'TRACE';		
+exports.logs_amount = '10';
+exports.log_size = '12400';
+exports.level = 'INFO';		
